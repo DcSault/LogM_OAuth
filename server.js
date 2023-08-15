341b8b72d13049e9f7d839eea837211f8f41101e
NotSault <rosiquevictor@gmail.com>
2023-08-15 15:36:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,61 +1,63 @@
+// ======== Modules ========
 const fs = require('fs');
 const crypto = require('crypto');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const dotenv = require('dotenv');
-const axios = require('axios');
 const Redis = require('ioredis');
 
 const app = express();
 const PORT = 443;
 
+// ======== Load Environment Variables ========
+dotenv.config({ path: './redis.env' });
+dotenv.config({ path: 'key.env' });
+dotenv.config({ path: 'ip.env' });
+
+const { MASTER_KEY, REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = process.env;
 
 // ======== Configuration Redis ========
-require('dotenv').config({ path: './redis.env' });
 const client = new Redis({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    password: process.env.REDIS_PASSWORD
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+    password: REDIS_PASSWORD
 });
 
-client.on('connect', function() {
+client.on('connect', () => {
     console.log('Connecté à Redis');
 });
-
-client.on('error', function(err) {
+client.on('error', err => {
     console.error('Erreur Redis:', err);
 });
 
+// ======== Master Key Utilities ========
 function saveMasterKeyToFile() {
     const masterKey = generateMasterKey();
     fs.writeFileSync('key.env', `MASTER_KEY=${masterKey}`);
     console.log('Clé maître sauvegardée dans key.env');
     return masterKey;
 }
-
 function generateMasterKey() {
     return crypto.randomBytes(32).toString('hex');
 }
-
 saveMasterKeyToFile();
 
-dotenv.config({ path: 'key.env' });
-const { MASTER_KEY } = process.env;
-
+// ======== Express Configuration ========
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.set('trust proxy', true);
 app.use(express.static('public'));
 
+// ======== Daily Code Utilities ========
 const codeLength = 6;
+let DAILY_CODE = generateDailyCode();
 
 function generateDailyCode() {
     const hash = crypto.createHash('sha256');
     hash.update(MASTER_KEY);
     const hashedValue = hash.digest('hex');
-
     let code = '';
     for (let i = 0; i < codeLength; i++) {
         const position = parseInt(hashedValue.slice(i * 2, i * 2 + 2), 16);
@@ -63,9 +65,6 @@ function generateDailyCode() {
     }
     return code;
 }
-
-let DAILY_CODE = generateDailyCode();
-console.log(`Code du jour : ${DAILY_CODE}`);
 
 function updateDailyCode() {
     DAILY_CODE = generateDailyCode();
@@ -85,59 +84,53 @@ function scheduleDailyCodeUpdate() {
 
 scheduleDailyCodeUpdate();
 
-dotenv.config({ path: 'ip.env' });
+// ======== Routes ========
 app.get('/', (req, res, next) => {
-    console.log('Requête reçue pour /code');
     const ipAddress = req.ip;
     const allowedIps = (process.env.ALLOWED_IPS || "").split(',');
+
     if (allowedIps.includes(ipAddress)) {
-        console.log(`IP autorisée : ${ipAddress}`);
-        
-        const now = new Date();
-        const midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0);
-        const timeUntilMidnight = midnight - now;
-        
-        const hoursLeft = Math.floor(timeUntilMidnight / (1000 * 60 * 60));
-        const minutesLeft = Math.floor((timeUntilMidnight % (1000 * 60 * 60)) / (1000 * 60));
-        
+        const timeUntilMidnight = calculateTimeUntilMidnight();
         res.render('code', { 
             code: DAILY_CODE,
-            hoursLeft: hoursLeft,
-            minutesLeft: minutesLeft
+            hoursLeft: timeUntilMidnight.hours,
+            minutesLeft: timeUntilMidnight.minutes
         });
     } else {
-        console.log(`Accès interdit depuis l'IP: ${ipAddress}`);
         next(new Error(`Accès interdit depuis l'IP: ${ipAddress}`));
     }
 });
 
 app.post('/verify', async (req, res, next) => {
-    console.log('Requête reçue pour vérification de code');
     const codeSent = req.body.code;
     if (codeSent === DAILY_CODE) {
-        console.log('Code validé');
-
         try {
             const jsonData = await client.get('errors');
             res.json({ valid: true, data: jsonData ? JSON.parse(jsonData) : {} });
         } catch (error) {
-            console.error('Erreur lors de la récupération du fichier JSON depuis Redis:', error.message);
             next(error);
         }
-
     } else {
-        console.log('Code invalide');
         res.json({ valid: false });
     }
 });
 
 app.use((err, req, res, next) => {
-    console.error(`Erreur: ${err.message}`);
-    if (!err.statusCode) err.statusCode = 500;
-    res.status(err.statusCode).send(err.message);
+    res.status(err.statusCode || 500).send(err.message);
 });
-
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
+
+// ======== Utility Functions ========
+function calculateTimeUntilMidnight() {
+    const now = new Date();
+    const midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0);
+    const timeUntilMidnight = midnight - now;
+    
+    return {
+        hours: Math.floor(timeUntilMidnight / (1000 * 60 * 60)),
+        minutes: Math.floor((timeUntilMidnight % (1000 * 60 * 60)) / (1000 * 60))
+    };
+}
